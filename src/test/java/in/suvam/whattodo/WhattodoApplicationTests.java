cdef6e7223ad686777a61d5d9f25e6fbec674fb6
Suvam Naskar <suvamnaskar780@gmail.com>
2023-10-25 08:00:51 +0530

    Create WhattodoApplicationTests.java

---
+++ src/test/java/in/suvam/whattodo/WhattodoApplicationTests.java
@@ -0,0 +1,13 @@
+package in.suvam.whattodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhattodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
