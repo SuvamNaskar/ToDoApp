e9708f0cd16d0df027558dc96dfbbcd9386bcaad
Suvam Naskar <suvamnaskar780@gmail.com>
2023-10-29 09:14:26 +0530

    Update allTask.java

--- src/main/java/in/suvam/whattodo/allTask.java
+++ src/main/java/in/suvam/whattodo/allTask.java
@@ -5,53 +5,45 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class allTask {
-    private String name;
-    private String time;
+    // String[] name = new String[10];
+    // String[] time;
+    public List<String> name=new ArrayList<String>();
+    public List<String> time=new ArrayList<String>();
+    int i = 0;
 
     private final String url = "jdbc:mysql://localhost:2023/ToDo";
     private final String username = "root";
     private final String password = "system32";
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getTime() {
-        return time;
-    }
-
-    public void setTime(String time) {
-        this.time = time;
-    }
-
     void fetchName()
     {
-        int loop = 1;
+        
         try
         {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection conn = DriverManager.getConnection(url, username, password);
             Statement stmt = conn.createStatement();
             ResultSet result = stmt.executeQuery("SELECT * FROM tasks;");
-            while(result.next())
+            for(i = 0; i < 10 && result.next(); i++)
             {
-                name = result.getString(2);
-                time = result.getString(3);
-                System.out.println(name + " " + time);
+                name.add(result.getString(2));
+                time.add(result.getString(3));
             }
-            loop = 0;
-
             conn.close();
         }
         catch(SQLException | ClassNotFoundException e)
         {
             e.printStackTrace();
+        }
+        for (String string : name) {
+            System.out.println(string);
+        }
+        for (String string : time) {
+            System.out.println(string);
         }
     }
 
